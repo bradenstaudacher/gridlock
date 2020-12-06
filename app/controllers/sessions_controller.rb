ea2caf30a04e05be57c85ef0d9fedea84b3fe747
Braden Staudacher <braden.staudacher@clio.com>
2020-12-06 11:43:20 -0800

    Add in user persistence

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,6 @@ class SessionsController < ApplicationController
   TOKEN_VERIFICATION_URL = "https://www.googleapis.com/oauth2/v1/tokeninfo"
 
   def google_auth
-    debugger
     access_token = request.env["omniauth.auth"]
     google_response_hash = request.env["omniauth.auth"].as_json
 
@@ -12,15 +11,18 @@ class SessionsController < ApplicationController
     access_token_valid?(uid: uid, access_token: access_token)
 
     user_info = google_response_hash["info"]
-
-    user_first_name = user_info["first_name"]
-    user_last_name = user_info["last_name"]
-    user_email = user_info["email"]
-    user_image = user_info["image"]
+    first_name = user_info["first_name"]
+    last_name = user_info["last_name"]
+    email = user_info["email"]
+    image = user_info["image"]
+    user = User.create!(uid: uid, access_token: access_token, first_name: first_name, last_name: last_name, email: email, image: image)
+    debugger
+    render status: 200, json: user.to_json
   end
 
   def access_token_valid?(uid:, access_token:)
-    verification_response = RestClient.get(TOKEN_VERIFICATION_URL, params: {access_token: token})
-    verification_response["expires_in"] > 0 && verification_response["user_id"] == uid
+    verification_response = RestClient.get(TOKEN_VERIFICATION_URL, params: {access_token: access_token})
+    parsed_response = JSON.parse(verification_response)
+    parsed_response["expires_in"] > 0 && parsed_response["user_id"] == uid
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,3 @@
 class User < ApplicationRecord
+  validates_presence_of :uid, :access_token, :first_name, :last_name, :email
 end
